1875a8b5a42db3d2d8098d3a843179574dee9b98
Mika <mikass111@yahoo.co.jp>
2019-11-08 16:46:15 -0800

    changed interface and how to get values from the player

--- Group5Chess/src/Chess/Interface/Display.java
+++ Group5Chess/src/Chess/Interface/Display.java
@@ -1,19 +1,17 @@
 package Chess.Interface;
 
 import Chess.Structure.Config;
+import Chess.Structure.Game;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.Style;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 import java.awt.*;
-import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
 
 public class Display {
     JFrame appFrame = new JFrame("Groupe 5 Chess");
     Screen screen = new Screen();
+    public JTextField currentField;
+    public JTextField nextField;
 
     private void setupFrame() {
         appFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -23,12 +21,35 @@ public class Display {
     }
 
     public Display() {
+        Action action = new AbstractAction()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                Game.getGameInstance().askMovement();
+            }
+        };
+        JPanel southPanel = new JPanel();
+        JLabel currentLabel = new JLabel("Current Position");
+        currentField = new JTextField(5);
+        southPanel.add(currentLabel);
+        southPanel.add(currentField);
+
+        JLabel nextLabel = new JLabel("Next Position");
+        nextField = new JTextField(5);
+        southPanel.add(nextLabel);
+        southPanel.add(nextField);
+
+        JButton button = new JButton("Move");
+        button.addActionListener( action );
+        southPanel.add(button);
+
         Container content = appFrame.getContentPane();
         Container topContainer = Box.createHorizontalBox();
         topContainer.add(screen);
 
         content.add(topContainer, BorderLayout.NORTH);
-        content.add(new JTextField(""), BorderLayout.SOUTH);
+        content.add(southPanel, BorderLayout.PAGE_END);
 
         setupFrame();
     }
