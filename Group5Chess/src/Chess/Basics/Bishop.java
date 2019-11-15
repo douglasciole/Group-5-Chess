d487b22c0bcaa7605db539358fb4d154d523c115
Bianca <biancacmatira@gmail.com>
2019-11-15 13:04:41 -0800

    Finished movements for Bishop & Queen

--- Group5Chess/src/Chess/Basics/Bishop.java
+++ Group5Chess/src/Chess/Basics/Bishop.java
@@ -26,11 +26,11 @@ public class Bishop extends Piece{
         String pos = "";
 
         for (int i = 0; i < 8; i++) {
-            if ((from.fixRow() + 1) + i > 7 || (from.getCol() + 1) + i > 7) {
+            if ((from.fixRow() + 1) + i > 8 || (from.getCol() + 1) + i > 7) {
                 break;
             }
 
-            pos = Character.toString(Config.letters.charAt((from.getCol() + 1) + i)) + (from.fixRow() + 1 + i);
+            pos = Character.toString(Config.letters.charAt((from.getCol() + 1) + i)) + ((from.fixRow() + 1) + i);
             if (board.getGrid().get(pos).isEmpty()) {
                 moves.add(pos);
             }else if (board.getGrid().get(pos).getPiece().getColor() != getColor()) {
@@ -42,11 +42,11 @@ public class Bishop extends Piece{
         }
 
         for (int i = 0; i < 8; i++) {
-            if ((from.fixRow() + 1) + i > 7 || (from.getCol() - 1) - i < 0) {
+            if ((from.fixRow() + 1) + i > 8 || (from.getCol() - 1) - i < 0) {
                 break;
             }
 
-            pos = Character.toString(Config.letters.charAt((from.getCol() - 1) - i)) + (from.fixRow() + 1 + i);
+            pos = Character.toString(Config.letters.charAt((from.getCol() - 1) - i)) + ((from.fixRow() + 1) + i);
             if (board.getGrid().get(pos).isEmpty()) {
                 moves.add(pos);
             }else if (board.getGrid().get(pos).getPiece().getColor() != getColor()) {
@@ -57,23 +57,36 @@ public class Bishop extends Piece{
             }
         }
 
-//        for (int i = 0; i < 8; i++) {
-//            if ((from.fixRow() + 1) + i > 7 || (from.getCol() - 1) - i < 0) {
-//                break;
-//            }
-//
-//            pos = Character.toString(Config.letters.charAt((from.getCol() - 1) - i)) + (from.fixRow() + 1 + i);
-//            moves.add(pos);
-//        }
-//
-//        for (int i = 0; i < 8; i++) {
-//            if ((from.fixRow() + 1) + i > 7 || (from.getCol() - 1) - i < 0) {
-//                break;
-//            }
-//
-//            pos = Character.toString(Config.letters.charAt((from.getCol() - 1) - i)) + (from.fixRow() + 1 + i);
-//            moves.add(pos);
-//        }
+        for (int i = 0; i < 8; i++) {
+            if ((from.fixRow() - 1) - i < 1 || (from.getCol() + 1) + i > 7) {
+                break;
+            }
+
+            pos = Character.toString(Config.letters.charAt((from.getCol() + 1) + i)) + ((from.fixRow() - 1) - i);
+            if (board.getGrid().get(pos).isEmpty()) {
+                moves.add(pos);
+            }else if (board.getGrid().get(pos).getPiece().getColor() != getColor()) {
+                moves.add(pos);
+                break;
+            }else if (board.getGrid().get(pos).getPiece().getColor() == getColor()) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < 8; i++) {
+            if ((from.fixRow() - 1) - i < 1 || (from.getCol() - 1) - i < 0) {
+                break;
+            }
+            pos = Character.toString(Config.letters.charAt((from.getCol() - 1) - i)) + ((from.fixRow() - 1) - i);
+            if (board.getGrid().get(pos).isEmpty()) {
+                moves.add(pos);
+            }else if (board.getGrid().get(pos).getPiece().getColor() != getColor()) {
+                moves.add(pos);
+                break;
+            }else if (board.getGrid().get(pos).getPiece().getColor() == getColor()) {
+                break;
+            }
+        }
 
         return moves;
     }
